26f25d0ade686d2cbeb81a31d1d8285bad9cd5f0
JAMMY <jeremyputradarma1@gmail.com>
2026-01-12 13:29:45 +0700

    Hapus folder duplikat, pindah semua file ke root

--- project berat badan/main.py
+++
@@ -1,9 +0,0 @@
-from beratbadan import BeratBadan
-from aktivitas_fisik import AktivitasFisik
-
-orang1 = BeratBadan(30, 170, "Laki-laki")
-orang2 = AktivitasFisik(30, 170, "Laki-laki")
-imt, kategori = orang1.menghitungbb()
-print(f"IMT: {imt}, Kategori: {kategori}")
-print(f"{orang1.rekomendasi_makanan()}")
-print(orang2.aktivitas_ringan())
--- project berat badan/userprofil.py
+++
@@ -1,2 +0,0 @@
-class UserProfil:
-    pass
