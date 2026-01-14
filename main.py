7f3abf3c9588ac4b9d0d715969bb5abb33cb12b6
JAMMY <jeremyputradarma1@gmail.com>
2026-01-14 22:31:19 +0700

    kerangka untuk user profil

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 from beratbadan import BeratBadan
 from aktivitas_fisik import AktivitasFisik
+from nutrisi import Nutrisi
 
 orang1 = BeratBadan(30, 170, "Laki-laki")
-orang2 = AktivitasFisik(30, 170, "Laki-laki")
+orang2 = Nutrisi(30, 170, "Laki-laki")
 imt, kategori = orang1.menghitungbb()
 print(f"IMT: {imt}, Kategori: {kategori}")
 # print(f"{orang1.rekomendasi_makanan()}")
 # print(orang2.aktivitas_ringan())
-print(orang2.membangun_masa_otot())
+print(orang2.bulking())
--- userprofil.py
+++ userprofil.py
@@ -1,2 +1,18 @@
 class UserProfil:
-    pass
+    def __init__(self, name, age, gender):
+        self.name = name
+        self.age = age
+        self.gender = gender
+        self.riwayat = []
+
+    def regist(self):
+        pass
+
+    def login(self):
+        pass
+
+    def cek_data(self):
+        pass
+
+    def cek_riwayat(self):
+        pass
