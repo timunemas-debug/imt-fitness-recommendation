d087b59b41b6a8e949c703afff1706d9657d51eb
JAMMY <jeremyputradarma1@gmail.com>
2026-01-19 19:24:39 +0700

    kerangka login

---
+++ login.py
@@ -0,0 +1,9 @@
+class Login:
+    def __init__(self):
+        pass
+    def tampilanlogin(self):
+        pass
+    def tampilankeluar(self):
+        pass
+    def tampilanregist(self):
+        pass
